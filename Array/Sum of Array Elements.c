4f65f674ecc933b43ce76b8017fe05be19e35fc1
MythiliMurugesan07 <mythilimurugesan12@gmail.com>
2026-01-22 20:58:50 +0530

    Sum of Array Elements.c

---
+++ Array/Sum of Array Elements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a[100], n, i, sum = 0;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+        sum += a[i];
+    }
+
+    printf("Sum = %d", sum);
+    return 0;
+}
