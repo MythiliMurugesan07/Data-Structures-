49f4f94222b8c87b4bd2a6dd5bd0962074ee01e1
Mythili_M07 <mythilimurugesan12@gmail.com>
2026-07-16 19:34:12 +0530

    welcome.java

---
+++ java/chapter1/welcome.java
@@ -0,0 +1,30 @@
+package chapter1;
+
+public class Welcome {
+	
+	public static void display() {
+		
+		System.out.println("Hello Java");
+		//return 100;
+	}
+
+	public static void main(String[] karthick) {
+		
+		int x = 100;
+		System.out.println("Welcome to java");
+		display(); //function call
+		System.out.println(x);		
+		System.out.println(stname);
+		
+	}
+	
+	static String stname  = "abc";
+	
+	public static void student_info() {
+		
+	}
+	
+	
+	//System.out.println("welcome to java");
+	
+}
