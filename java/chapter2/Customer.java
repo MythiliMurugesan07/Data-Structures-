5b110764daad28b7b059c385d63496761cd2f6c7
Mythili_M07 <mythilimurugesan12@gmail.com>
2026-07-18 23:35:22 +0530

    Customer.java

---
+++ java/chapter2/Customer.java
@@ -0,0 +1,36 @@
+package chapter2;
+
+public class Customer {
+
+	private String cust_id;
+	private String cust_name;
+	private int qty;
+	private double price;
+	
+	public String getCust_id() {
+		return cust_id;
+	}
+	public void setCust_id(String cust_id) {
+		this.cust_id = cust_id;
+	}
+	public String getCust_name() {
+		return cust_name;
+	}
+	public void setCust_name(String cust_name) {
+		this.cust_name = cust_name;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	
+	
+}
